201494d2d3a61ef4c9f089c0a229cb8bbea4ff82
Igor Chernuha <44615669+xSharknadox@users.noreply.github.com>
2019-08-07 22:16:35 +0300

    Revert "Optional in exceptions"

--- src/com/company/exceptions/DontHaveFailableOfChildException.java
+++ src/com/company/exceptions/DontHaveFailableOfChildException.java
@@ -1,14 +1,8 @@
 package com.company.exceptions;
 
 public class DontHaveFailableOfChildException extends RuntimeException {
-    private String message;
-
-    public DontHaveFailableOfChildException(String message) {
-        this.message = message;
-    }
-
     @Override
     public String getMessage() {
-        return "Don't have a Failable of child. " + message;
+        return "Don't have a Failable of child.";
     }
 }
